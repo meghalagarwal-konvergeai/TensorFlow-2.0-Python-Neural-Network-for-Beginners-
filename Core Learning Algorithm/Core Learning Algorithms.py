65be36af7c6ba9342490adb44cf2e83fecaa2802
meghalagarwal-konvergeai <meghal.agarwal@konverge.ai>
2022-05-11 20:04:29 +0530

    Core Learning Algorithm using TensorFlow Tutorial for Beginners

---
+++ Core Learning Algorithm/Core Learning Algorithms.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+from six.moves import urllib
+import tensorflow.compat.v2.feature_column as fc
+import tensorflow as tf
+
+# Load dataset.
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
+
+dftrain.head()
+dftrain.describe()
+dftrain.shape
+y_train.head()
+dftrain.age.hist(bins=20)
+# plt.show()
+dftrain.sex.value_counts().plot(kind='barh')
+# plt.show()
+dftrain['class'].value_counts().plot(kind='barh')
+# plt.show()
+pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive')
+# plt.show()
+
+# print(dftrain.dtypes)
+
+# Creating a List of Categorical and Numerical columns
+CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck',
+                       'embark_town', 'alone']
+NUMERIC_COLUMNS = ['age', 'fare']
+
+feature_columns = []
+# Storing the Unique values of each column along with the data types in a list
+for feature_name in CATEGORICAL_COLUMNS:
+  vocabulary = dftrain[feature_name].unique()  # gets a list of all unique values from given feature column
+  feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+for feature_name in NUMERIC_COLUMNS:
+  feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+print(feature_columns)
+
+# Creating an input function to divide the data into a batch of 32 with epoch 10
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+  def input_function():  # inner function, this will be returned
+    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))  # create tf.data.Dataset object with data and its label
+    if shuffle:
+      ds = ds.shuffle(1000)  # randomize order of data
+    ds = ds.batch(batch_size).repeat(num_epochs)  # split dataset into batches of 32 and repeat process for number of epochs
+    return ds  # return a batch of the dataset
+  return input_function  # return a function object for use
+
+train_input_fn = make_input_fn(dftrain, y_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+# We create a linear estimtor by passing the feature columns we created earlier
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+
+linear_est.train(train_input_fn)  # train
+result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on tetsing data
+
+clear_output()  # clears consoke output
+print(result['accuracy'])  # the result variable is simply a dict of stats about our model
+print(result)
